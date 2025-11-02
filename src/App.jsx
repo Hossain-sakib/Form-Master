3ea5d1bba25ebd93f4834288d67b8df9000ffbbc
Hossain-sakib <Hossain.sakib.x@gmail.com>
2025-11-03 02:17:03 +0600

    Completed the project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import SimpleForm from "./Components/SimpleForm/SimpleForm";
+function App() {
+  return (
+    <>
+      <div className="w-4xl mx-auto text-center">
+        <SimpleForm></SimpleForm>
+      </div>
+    </>
+  );
+}
+
+export default App;
